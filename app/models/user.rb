e7c72482af197a1d598e1aa016f4be7f828a1a5b
Gennadiy <genaydzhan70@gmail.com>
2019-03-31 20:04:38 +0600

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :riddles
+  validates :name, presence: true, length: {maximum: 30}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+            format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
 end
