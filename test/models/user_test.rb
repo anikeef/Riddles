0a2dedc3638dc83440f17a49ab37600090ae8410
Gennadiy <genaydzhan70@gmail.com>
2019-04-01 17:53:18 +0600

    Add tests for user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "email@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test "name presence" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "name length" do
+    @user.name = "a" * 31
+    assert_not @user.valid?
+  end
+
+  test "password presence" do
+    @user.password = @user.password_confirmation = ""
+    assert_not @user.valid?
+  end
+
+  test "password length" do
+    @user.password = @user.password_confirmation = "foo"
+    assert_not @user.valid?
+  end
+
+  test "password confirmation" do
+    @user.password_confirmation = "barbaz"
+    assert_not @user.valid?
+  end
+
+  test "email presence" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "email format" do
+    @user.email = "ivalid@email"
+    assert_not @user.valid?
+    @user.email = "@invalid.com"
+    assert_not @user.valid?
+  end
 end
