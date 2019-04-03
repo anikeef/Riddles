f9ccf534511eb00aa605f204c143b7a37c337817
Gennadiy <genaydzhan70@gmail.com>
2019-04-03 17:00:25 +0600

    Add pg gem to deploy to Heroku

--- test/controllers/riddles_controller_test.rb
+++ test/controllers/riddles_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RiddlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get riddles_index_url
+    get riddles_url
     assert_response :success
   end
 
