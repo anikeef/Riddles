bae7e64e990f842bd2af2302f92710945a385e08
Gennadiy <genaydzhan70@gmail.com>
2019-04-05 20:17:00 +0600

    Create basic machinery for creating riddles

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -1,5 +1,32 @@
 class RiddlesController < ApplicationController
+  before_action :logged_in, only: [:new, :create]
+
   def index
     @riddles = Riddle.all
+  end
+
+  def new
+    @riddle = Riddle.new
+  end
+
+  def create
+    @riddle = Riddle.new(riddle_params)
+    @riddle.user = current_user
+    if @riddle.save
+      flash[:success] = "Your riddle has been published"
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def riddle_params
+    params.require(:riddle).permit(:body, :answer, :user)
+  end
+
+  def logged_in
+    redirect_to root_url unless logged_in?
   end
 end
