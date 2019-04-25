11db3697002514bac84c22390f9bfbd351776268
Gennadiy <genaydzhan70@gmail.com>
2019-04-25 17:40:09 +0600

    Add sample avatar to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
+avatar = File.open("test/fixtures/files/avatar.jpg")
+
 User.create!(name: "Example",
   email: "foo@bar.com",
   password: "foobar",
-  password_confirmation: "foobar")
+  password_confirmation: "foobar",
+  avatar: avatar)
 
 5.times do |n|
   user = User.create!(name: "Example#{n}",
     email: "foo#{n}@bar.com",
     password: "foobar",
-    password_confirmation: "foobar")
+    password_confirmation: "foobar",
+    avatar: avatar)
   user.problems.create!(body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
     answer: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore")
 end
