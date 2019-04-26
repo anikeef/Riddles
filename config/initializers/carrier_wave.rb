74091a6fb9b3a1057758d9bace0a20d6d594080e
Gennadiy <genaydzhan70@gmail.com>
2019-04-26 22:08:48 +0600

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = "fog/aws"
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["S3_ACCESS_KEY"],
@@ -7,5 +8,7 @@ if Rails.env.production?
       aws_region: ENV["S3_REGION"]
     }
     config.fog_directory = ENV["S3_BUCKET"]
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+    config.storage = :fog
   end
 end
