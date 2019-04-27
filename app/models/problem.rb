6fa2d93bc8bda680772dc9ffe3ded679b3409774
Gennadiy <genaydzhan70@gmail.com>
2019-04-27 14:40:31 +0600

    Allow creating problems without text

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,17 @@
 class Problem < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  validates :body, presence: true
-  validates :answer, presence: true
+  validate :restrict_blank_problem
+
+  def answer?
+    !answer.blank?
+  end
+
+  private
+
+  def restrict_blank_problem
+    if body.blank? && !picture?
+      errors.add(:body, "can't be blank")
+    end
+  end
 end
