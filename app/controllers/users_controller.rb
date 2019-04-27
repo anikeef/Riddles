ecf021a6d63854d8aac36ed62d5b1ed73c2cdcb8
Gennadiy <genaydzhan70@gmail.com>
2019-04-27 15:58:56 +0600

    Show user page only when logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: :index
+  before_action :logged_in_user, only: [:index, :show]
   before_action :logged_out_user, only: :new
 
   def new
