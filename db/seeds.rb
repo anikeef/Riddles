232907061cf74abf32f3bd937ccb37dbbc6d9dc0
Gennadiy <genaydzhan70@gmail.com>
2019-04-28 22:12:19 +0600

    Activate users in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,16 @@ User.create!(name: "Example",
   email: "foo@bar.com",
   password: "foobar",
   password_confirmation: "foobar",
-  avatar: avatar)
+  avatar: avatar,
+  activated: true)
 
 5.times do |n|
   user = User.create!(name: "Example#{n}",
     email: "foo#{n}@bar.com",
     password: "foobar",
     password_confirmation: "foobar",
-    avatar: avatar)
+    avatar: avatar,
+    activated: true)
   user.problems.create!(body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
     answer: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore")
 end
