c0db53ebfdf812094ab83a11719e21db8e3e881b
Gennadiy <genaydzhan70@gmail.com>
2019-04-28 18:32:32 +0600

    Fix host name in smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'anikeef-sample-app.herokuapp.com'
+  host = 'anikeef-problems.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
